2a12fdcb5ff21ea48d92bfa35a592d8bdeb778a9
Marcus Waibel <waibelmarcus@gmail.com>
2024-02-23 22:48:26 +0100

    Fix QR decoding

--- scripts/qr_code.py
+++ scripts/qr_code.py
@@ -1,6 +1,5 @@
 import cv2
 from pyzbar.pyzbar import decode
-from PIL import Image
 import numpy as np
 
 
@@ -10,6 +9,7 @@ def main():
 
     while True:
         _, img = cap.read()
+
         bbox = None
         code = None
         try:
@@ -17,17 +17,11 @@ def main():
         except:
             print("cv2 der hurensohn")
 
-        print("Detection:", bbox, code)
-
         if bbox is not None:
             for i in range(len(bbox)):
-                print("Bbox", bbox)
-                print("Tuple 1", tuple(bbox[i][0]))
-                print("Tuple 2", tuple(bbox[i][2]))
                 cv2.rectangle(img, (int(bbox[i][0][0]), int(bbox[i][0][1])), (int(bbox[i][2][0]), int(bbox[i][2][1])),
                               (255, 0, 255), 2)
             if code is not None:
-                print("data found: ", code)
                 print(decode_qr_from_matrix(code))
 
         cv2.imshow("code detector", img)
@@ -37,27 +31,19 @@ def main():
     cv2.destroyAllWindows()
 
 
-def matrix_to_qr_image(matrix):
-    size = len(matrix)
-    # Create a new black/white image
-    img = Image.new('L', (size, size), 1)  # '1' for 1-bit pixels, white background
-    pixels = img.load()
-    for i in range(size):
-        for j in range(size):
-            if matrix[i][j] == 255:
-                pixels[i, j] = 255
-            else:
-                pixels[i, j] = 0
-    return img
+def matrix_to_cv2_img(matrix):
+    return np.array(matrix, dtype=np.uint8)
+
+
+def scale_image_up(img, factor):
+    height, width = img.shape
+    return cv2.resize(img, (height * factor, width * factor), interpolation=cv2.INTER_AREA)
 
 
 def decode_qr_from_matrix(matrix):
-    img = matrix_to_qr_image(matrix)
-    print("Image:", img)
-    img.show()
-    quit(0)
-    decoded_objects = decode(cvimage)
-    print("Decoded objects:", decoded_objects)
+    img = matrix_to_cv2_img(matrix)
+    img_scaled = scale_image_up(img, 40) # fucking pyzbar ist weitsichtig
+    decoded_objects = decode(img_scaled)
     if decoded_objects:
         return decoded_objects[0].data.decode()  # Assuming only one QR code in the image
     else:
