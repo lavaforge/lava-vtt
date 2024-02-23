c0033b2b6e9e8b346cbdc8e8d40f10e96aa877e9
Philipp Scheede <github@p-scheede.de>
2024-02-23 23:36:19 +0100

    save images in mongodb by hash

--- apps/obsidian-plugin/main.ts
+++ apps/obsidian-plugin/main.ts
@@ -1,12 +1,16 @@
-import { FileSystemAdapter, Menu, Plugin, TFile } from "obsidian";
+import { FileSystemAdapter, Menu, Plugin, TFile } from 'obsidian';
+import { promises as fs } from 'fs';
+import { hashBuffer } from 'shared';
 
 // Remember to rename these classes and interfaces!
 
 export default class MyPlugin extends Plugin {
+  private apiVersion: string | undefined = undefined;
+
   async onload() {
-    this.registerDomEvent(document, "contextmenu", (event: MouseEvent) => {
+    this.registerDomEvent(document, 'contextmenu', (event: MouseEvent) => {
       const target = event.target as HTMLElement;
-      if (target.localName !== "img") return;
+      if (target.localName !== 'img') return;
 
       const imgPath = (target as HTMLImageElement).currentSrc;
       // @ts-expect-error
@@ -19,15 +23,65 @@ export default class MyPlugin extends Plugin {
 
       const menu = new Menu();
       menu.addItem((item) => {
-        item.setTitle(`Open ${adapter.getFullPath(file.path)}`).onClick(() =>
-          fetch("http://localhost:3000/api/new-image", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ path: adapter.getFullPath(file.path) }),
-          }),
-        );
+        item
+          .setTitle(`Open ${adapter.getFullPath(file.path)}`)
+          .onClick(async () => {
+            if (this.apiVersion === undefined) {
+              // fetch from /api/version. if route is not found, use version v0
+              await fetch('http://localhost:3000/api/version')
+                .then((res) => res.json())
+                .then((data) => {
+                  this.apiVersion = data.version;
+                  console.log('API VERSION', this.apiVersion);
+                })
+                .catch(() => {
+                  this.apiVersion = 'v0';
+                  console.log('API VERSION', this.apiVersion);
+                });
+            }
+
+            if (this.apiVersion === 'v0') {
+              return fetch('http://localhost:3000/api/new-image', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ path: adapter.getFullPath(file.path) }),
+              });
+            } else if (this.apiVersion === 'v1') {
+              const fileContent = await fs.readFile(
+                adapter.getFullPath(file.path),
+              );
+
+              const hash = hashBuffer(fileContent);
+              console.log('other hash', hash);
+
+              const headResponse = await fetch(
+                `http://localhost:3000/api/image/${hash}`,
+                {
+                  method: 'HEAD',
+                },
+              );
+
+              if (headResponse.status !== 200) {
+                console.log('uploading new image');
+                await fetch('http://localhost:3000/api/image', {
+                  method: 'POST',
+                  body: fileContent, // Send the file content as the body
+                  headers: {
+                    // Set appropriate headers if needed (e.g., for file type)
+                    'Content-Type': 'application/octet-stream',
+                  },
+                });
+              } else {
+                console.log('image already exists, no upload necessary');
+              }
+            } else {
+              throw new Error(
+                'Unknown API version, please update this plugin!',
+              );
+            }
+          });
       });
 
       menu.showAtPosition({ x: event.pageX, y: event.pageY });
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,18 +1,18 @@
-import express from "express";
-import { Server } from "socket.io";
-import { createServer } from "http";
-import path from "path";
-import fs from "fs/promises";
-import { MongoClient } from "mongodb";
+import express from 'express';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+import { MongoClient } from 'mongodb';
+import { apiRouter } from './routes/api.route';
+import { ServiceContainer } from 'ioc-service-container';
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept",
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept',
   );
   next();
 });
@@ -22,116 +22,64 @@ const httpServer = createServer(app);
 const io = new Server(httpServer);
 
 const fogOfWars = {};
-let currentImage = "";
+const currentImage = '';
 
-io.on("connection", (socket) => {
-  console.log("A user connected");
+io.on('connection', (socket) => {
+  console.log('A user connected');
   console.log(fogOfWars, currentImage);
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
   });
 
   if (fogOfWars[currentImage]) {
     setTimeout(
-      () => socket.emit("fow-broadcast", fogOfWars[currentImage]),
+      () => socket.emit('fow-broadcast', fogOfWars[currentImage]),
       100,
     );
   }
 
-  socket.on("new-fow", (msg) => {
+  socket.on('new-fow', (msg) => {
     fogOfWars[currentImage] = msg;
-    io.emit("fow-broadcast", msg);
+    io.emit('fow-broadcast', msg);
   });
 
-  socket.on("loaded", () => {
+  socket.on('loaded', () => {
     if (fogOfWars[currentImage]) {
-      socket.emit("fow-broadcast", fogOfWars[currentImage]);
+      socket.emit('fow-broadcast', fogOfWars[currentImage]);
     }
   });
 });
 
-app.post("/api/new-image", async (req, res) => {
-  const newImage = req.body.path;
-  currentImage = newImage;
-
-  try {
-    await fs.copyFile(
-      newImage,
-      path.join(path.resolve("apps/client"), assetPath(), "current_image"),
-    );
-  } catch (e) {
-    console.log(e);
-  }
-
-  io.emit("new-image", "new image");
-
-  res.json({ body: req.body });
-});
-
-const homepageRouter = express.Router();
-
-const environment = process.env.NODE_ENV;
-const assetPath = () => (environment === "production" ? "dist" : "public");
-
-homepageRouter.get("/", async (req, res) => {
-  const data = {
-    environment,
-    manifest: await parseManifest(),
-  };
-
-  res.render("index.html.ejs", data);
-});
-
-homepageRouter.get("/image", async (req, res) => {
-  res.sendFile(
-    path.join(path.resolve("apps/client"), assetPath(), "current_image"),
-  );
-});
-
-const parseManifest = async () => {
-  if (environment !== "production") return {};
-
-  const manifestPath = path.join(path.resolve(), "dist", "manifest.json");
-  const manifestFile = await fs.readFile(manifestPath);
-
-  return JSON.parse(manifestFile.join(""));
-};
-
-app.use(homepageRouter);
+app.use('/api', apiRouter);
 
 // Connection URL
-const url = "mongodb://rootuser:rootpass@localhost:27017";
+const url = 'mongodb://rootuser:rootpass@localhost:27017';
 // Replace 'myDatabase' with your database name
-const dbName = "myDatabase";
+const dbName = 'lava-vtt-db';
 
-fs.copyFile(
-  path.join(path.resolve("apps/client"), assetPath(), "current_image.bkp"),
-  path.join(path.resolve("apps/client"), assetPath(), "current_image"),
-).then(() =>
-  httpServer.listen(port, async () => {
-    const client = new MongoClient(url);
+const client = new MongoClient(url);
+httpServer.listen(port, async () => {
+  try {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
 
-    try {
-      // Use connect method to connect to the server
-      await client.connect();
-      console.log("Connected successfully to server");
+    const db = client.db(dbName);
+    ServiceContainer.set('Db', () => db);
 
-      const db = client.db(dbName);
+    // Your database interaction code here
+    // For example, list the available collections
+    const collections = await db.listCollections().toArray();
+    console.log(
+      'Collections:',
+      collections.map((col) => col.name),
+    );
+  } catch (err) {
+    console.error('Connection failed', err);
+  } finally {
+    // Ensure that the client will close when you finish/error
+    // await client.close();
+  }
 
-      // Your database interaction code here
-      // For example, list the available collections
-      const collections = await db.listCollections().toArray();
-      console.log(
-        "Collections:",
-        collections.map((col) => col.name),
-      );
-    } catch (err) {
-      console.error("Connection failed", err);
-    } finally {
-      // Ensure that the client will close when you finish/error
-      await client.close();
-    }
-
-    console.log("listening on *:3000");
-  }),
-);
+  console.log('listening on *:3000');
+});
