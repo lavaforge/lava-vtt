a2ee57bf23cf4f6a7a77c07926fb99abd2fed08a
Marcus Waibel <waibelmarcus@gmail.com>
2024-02-27 22:45:07 +0100

    Remove the need for cv2.QRCodeDetector so self compiling opencv is not needed anymore on raspberry

--- scripts/qr_code.py
+++ scripts/qr_code.py
@@ -1,8 +1,6 @@
 import re
-
 import cv2
 from pyzbar.pyzbar import decode
-import numpy as np
 import netifaces as net
 import ipaddress
 
@@ -105,52 +103,33 @@ def connect_wifi(ssid, password):
     # TODO: implement
 
 
-def matrix_to_cv2_img(matrix):
-    return np.array(matrix, dtype=np.uint8)
-
-
-def scale_image_up(img, factor):
-    height, width = img.shape
-    return cv2.resize(img, (height * factor, width * factor), interpolation=cv2.INTER_AREA)
-
-
-def decode_qr_from_matrix(matrix):
-    img = matrix_to_cv2_img(matrix)
-    img_scaled = scale_image_up(img, 40)  # fucking pyzbar ist weitsichtig
-    decoded_objects = decode(img_scaled)
-    if decoded_objects:
+def decode_qr_from_image(img):
+    decoded_objects = decode(img)
+    if (decoded_objects):
         return decoded_objects[0].data.decode()
     else:
         return NO_QR
 
 
+def scale_image_down(img):
+    return cv2.resize(img, (int(img.shape[1] * 0.5), int(img.shape[0] * 0.5)), interpolation=cv2.INTER_AREA)
+
+
 def read_qr_code():
     cap = cv2.VideoCapture(0)
-    detector = cv2.QRCodeDetector()
-
-    result = ""
-
     while True:
         _, img = cap.read()
 
-        bbox = None
-        code = None
-        try:
-            _, bbox, code = detector.detectAndDecodeCurved(img)
-        except:
-            print("cv2 der hurensohn")
+        img = scale_image_down(img)
 
-        if bbox is not None:
-            for i in range(len(bbox)):
-                cv2.rectangle(img, (int(bbox[i][0][0]), int(bbox[i][0][1])), (int(bbox[i][2][0]), int(bbox[i][2][1])),
-                              (255, 0, 255), 2)
-            if code is not None:
-                result = decode_qr_from_matrix(code)
-                if result != NO_QR:
-                    break
-        cv2.imshow("code detector", img)
+        result = decode_qr_from_image(img)
+        if result != NO_QR:
+            break
+
+        cv2.imshow("Image", img)
         if cv2.waitKey(1) == ord("q"):
             break
+
     cap.release()
     cv2.destroyAllWindows()
     return result
