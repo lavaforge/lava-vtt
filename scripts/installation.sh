f18610c3e48379d64ffe16a7b431f842ecdf1550
Marcus Waibel <waibelmarcus@gmail.com>
2024-02-27 22:45:07 +0100

    Remove the need for cv2.QRCodeDetector so self compiling opencv is not needed anymore on raspberry

---
+++ scripts/installation.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get upgrade
+# TODO: enable vnc, ssh, and legacy camera
+
+sudo apt-get install libopencv-dev
+sudo apt-get install python3-libopencv
+
+pip install pyzbar
+pip install netifaces
