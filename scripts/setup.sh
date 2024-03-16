dd1847fbce191a1170543656d4d033ff0fc682a3
Marcus Waibel <waibelmarcus@gmail.com>
2024-03-16 16:00:47 +0100

    Changed branch back to main in script download url

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -71,7 +71,7 @@ echo "Creating lava directory"
 mkdir -p /lava
 cd /lava || exit
 
-if wget -O qr_code.py https://raw.githubusercontent.com/lavaforge/lava-vtt/scriptDebug/scripts/qr_code.py; then # TODO: change branch again
+if wget -O qr_code.py https://raw.githubusercontent.com/lavaforge/lava-vtt/main/scripts/qr_code.py; then
     echo "Download successful. Script updated or downloaded."
 else
     echo "Error downloading script. Please check your internet connection or the URL."
