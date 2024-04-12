395d13813587e6383f4cfcbf73dd4f66d27bbf52
Marcus Waibel <waibelmarcus@gmail.com>
2024-04-12 18:23:00 +0200

    Make simple fow functional with paper js

--- apps/client/src/components/DmView.vue
+++ apps/client/src/components/DmView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed, nextTick, ref } from 'vue';
 import { useSocket } from '../logic/useSocket';
 import { useEventListener } from '@vueuse/core';
 import { FogOfWar } from '../logic/FogOfWar';
 import { useMouse } from '@vueuse/core';
 import { scg } from 'ioc-service-container';
+import paper from 'paper';
+import { project } from 'paper/dist/paper-core';
 
 const { emit } = useSocket({
   event: 'new-image',
@@ -14,14 +16,97 @@ const { emit } = useSocket({
 });
 
 const apiUrl = scg('apiUrl');
-
 const hash = ref('');
 const imagePath = computed(() => `${apiUrl}/api/image/${hash.value}`);
-
-// log image dimensions once every second
+const canvasRef = ref<HTMLCanvasElement | null>(null);
 const imageRef = ref<HTMLImageElement | null>(null);
 const width = ref(0);
 const height = ref(0);
+const addFow = ref(false);
+const { x: mouseX, y: mouseY } = useMouse();
+
+function initPaper() {
+  if (canvasRef.value) {
+    paper.setup(canvasRef.value);
+    initDrawingTools();
+  }
+}
+
+function initDrawingTools() {
+  // TODO: also set up drawing tools for freehand drawing
+  const tool = new paper.Tool();
+
+  console.log('init tool');
+
+  let path: paper.Path;
+  tool.onMouseDown = function (event: {
+    point: paper.Segment | paper.PointLike | number[];
+  }) {
+    console.log('mouse down detected');
+    path = new paper.Path();
+    if (addFow.value) {
+      path.strokeColor = new paper.Color('black');
+    } else {
+      path.strokeColor = new paper.Color('red');
+    }
+    path.add(event.point);
+  };
+
+  tool.onMouseDrag = function (event: {
+    point: number[] | paper.Segment | paper.PointLike;
+  }) {
+    if (path) {
+      console.log('drag');
+      path.add(event.point);
+    }
+  };
+
+  tool.onMouseUp = function () {
+    console.log('mouse up');
+    if (path) {
+      path.closed = true;
+      path.simplify();
+
+      if (addFow.value) {
+        let combinedPath: paper.Path | paper.PathItem = path;
+        paper.project.activeLayer.children.forEach((child) => {
+          console.log('child: ' + child + ' with type ' + child.className);
+          if (
+            child != path &&
+            (child instanceof paper.Path ||
+              child instanceof paper.CompoundPath ||
+              child instanceof paper.PathItem)
+          ) {
+            combinedPath = combinedPath.unite(child);
+          }
+        });
+        combinedPath.fillColor = new paper.Color('black');
+        paper.project.activeLayer.removeChildren();
+        paper.project.activeLayer.addChild(combinedPath);
+      } else {
+        let substractedPath:
+          | paper.CompoundPath
+          | paper.PathItem
+          | paper.Path
+          | paper.Item = path;
+        paper.project.activeLayer.children.forEach((child) => {
+          if (
+            (child instanceof paper.CompoundPath ||
+              child instanceof paper.Path ||
+              child instanceof paper.PathItem) &&
+            child != path
+          ) {
+            substractedPath = child.subtract(path);
+          }
+        });
+        paper.project.activeLayer.removeChildren();
+        substractedPath.fillColor = new paper.Color('black');
+        paper.project.activeLayer.addChild(substractedPath);
+      }
+    }
+  };
+  tool.activate();
+}
 
 useEventListener(imageRef, 'load', async () => {
   if (!imageRef.value || !canvasRef.value) {
@@ -36,143 +121,15 @@ useEventListener(imageRef, 'load', async () => {
     throw new Error('no ctx');
   }
 
-  off?.();
-
-  console.log('before fow', width.value, height.value);
-  fow = new FogOfWar(ctx, width.value, height.value, true);
-  try {
-    await fetch(`${apiUrl}/api/fow/${hash.value}`)
-      .then((res) => res.json())
-      .then((data) => {
-        if (data) {
-          console.log('setting data', data);
-          fow?.setData(data);
-          fow?.update();
-          console.log('initial update');
-        }
-      });
-  } catch (e) {
-    console.error(e);
-  }
-
-  console.log('after fow fetch');
-
-  console.log(fow.width, fow.height);
-  ({ off } = fow.onUpdate((data) => {
-    console.warn('new fow');
-    emit('new-fow', data);
-  }));
-
-  fow.update(true);
-});
-
-let fow: FogOfWar | null = null;
-let off: ReturnType<FogOfWar['onUpdate']>['off'] | null = null;
-
-const canvasRef = ref<HTMLCanvasElement | null>(null);
-
-type Point = { x: number; y: number };
-
-let startPoint: Point | null = null;
-let mouseStart: Point | null = null;
-useEventListener(canvasRef, 'mousedown', (e) => {
-  if (!canvasRef.value) {
-    return;
-  }
-
-  startPoint = getPointOnCanvas(
-    { x: e.clientX, y: e.clientY },
-    canvasRef.value,
-  );
-  mouseStart = { x: mouseX.value, y: mouseY.value };
-});
-
-function getPointOnCanvas(p: Point, canvas: HTMLCanvasElement): Point {
-  // Get the bounding rectangle of the canvas
-  const rect = canvas.getBoundingClientRect();
-
-  // Calculate the scaling factors
-  const scaleX = canvas.width / rect.width;
-  const scaleY = canvas.height / rect.height;
-
-  // Adjust mouse event coordinates
-  const canvasX = Math.floor((p.x - rect.left) * scaleX);
-  const canvasY = Math.floor((p.y - rect.top) * scaleY);
-
-  return { x: canvasX, y: canvasY };
-}
-
-useEventListener(canvasRef, 'mouseup', (e) => {
-  if (!canvasRef.value || !startPoint) {
-    return;
-  }
-
-  const end = getPointOnCanvas({ x: e.clientX, y: e.clientY }, canvasRef.value);
-
-  console.log(startPoint, end);
-  const [topLeft, bottomRight] = getTopLeftAndBottomRight(startPoint, end);
-  paintRectangle(topLeft, bottomRight);
-
-  startPoint = null;
-  if (rectangleRef.value) {
-    rectangleRef.value.style.display = 'none';
-  }
-});
-
-const shouldCover = ref(false);
-function paintRectangle(topLeft: Point, bottomRight: Point) {
-  if (!fow) {
-    return;
-  }
-
-  const lines: Array<[{ x: number; y: number }, number]> = [];
-  for (let y = topLeft.y; y < bottomRight.y; y++) {
-    lines.push([{ x: topLeft.x, y }, bottomRight.x - topLeft.x]);
-  }
-
-  console.log('painting');
-  lines.forEach(([start, length]) => {
-    fow?.drawRunInSingleLine(shouldCover.value, start, length);
-  });
-
-  console.log('updating');
-  fow.update();
-  console.log('updated');
-}
-
-function getTopLeftAndBottomRight(start: Point, end: Point): [Point, Point] {
-  return [
-    { x: Math.min(start.x, end.x), y: Math.min(start.y, end.y) },
-    { x: Math.max(start.x, end.x), y: Math.max(start.y, end.y) },
-  ];
-}
-
-const rectangleRef = ref<HTMLDivElement | null>(null);
-useEventListener(canvasRef, 'mousemove', () => {
-  if (!startPoint || !mouseStart || !rectangleRef.value) {
-    return;
-  }
-
-  const currentX = mouseX.value;
-  const currentY = mouseY.value;
-
-  const width = Math.abs(currentX - mouseStart.x);
-  const height = Math.abs(currentY - mouseStart.y);
-
-  rectangleRef.value.style.left = `${Math.min(currentX, mouseStart.x)}px`;
-  rectangleRef.value.style.top = `${Math.min(currentY, mouseStart.y)}px`;
-  rectangleRef.value.style.width = `${width}px`;
-  rectangleRef.value.style.height = `${height}px`;
-  rectangleRef.value.style.display = 'block';
+  await nextTick();
+  initPaper();
 });
 
 useEventListener('keydown', (e: KeyboardEvent) => {
   if (e.key === 'x') {
-    shouldCover.value = !shouldCover.value;
+    addFow.value = !addFow.value;
   }
 });
-
-const { x: mouseX, y: mouseY } = useMouse();
 </script>
 
 <template>
@@ -183,7 +140,7 @@ const { x: mouseX, y: mouseY } = useMouse();
       <div ref="rectangleRef" class="rectangle" />
       <div
         class="indicator"
-        :style="{ 'background-color': shouldCover ? 'black' : 'white' }"
+        :style="{ 'background-color': addFow ? 'black' : 'white' }"
       />
       <div class="vertical-line" :style="{ left: mouseX + 'px' }"></div>
       <div class="horizontal-line" :style="{ top: mouseY + 'px' }"></div>
